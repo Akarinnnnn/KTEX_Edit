b7ccddbf4b73d274742b95e58b6d05c7bbb7e14d
Fa鸽 <1207247008@qq.com>
2019-07-16 16:49:25 +0800

    添加项目文件。

---
+++ KTEX Convert/Bootstrap.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Windows;
+using System.Text;
+
+namespace KTEX.KTEXConvert
+{
+	public static class Bootstrap
+	{
+		[STAThread]
+		static int Main(string[] args)
+		{
+			MainWindow main = new MainWindow();
+			App app = new App();
+			return app.Run();
+		}
+	}
+}
